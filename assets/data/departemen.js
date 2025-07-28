7127d334d3e817abd453e64736dab30dc4b40161
HengkyLaurencio <liu.hengky.laurencio@gmail.com>
2025-07-28 19:52:50 +0700

    Add departemen data

---
+++ assets/data/departemen.js
@@ -0,0 +1,91 @@
+const departemen = [
+    {
+        nama: "BPHI",
+        deskripsi: "Badan Pengurus Harian Inti",
+        anggota: [
+            { nama: "Nanda Goaw Putra", jabatan: "Ketua Umum", foto: "assets/img/anggota/Nanda.jpg" },
+            { nama: "Steven Flianto", jabatan: "Wakil Internal", foto: "assets/img/anggota/Steven.jpg" },
+            { nama: "Yudo Alfariszi", jabatan: "Wakil Eksternal", foto: "assets/img/anggota/Yudo.jpg" },
+            { nama: "Carissa Puteri", jabatan: "Sekretaris Umum", foto: "assets/img/anggota/Carissa.jpg" },
+            { nama: "Shareen Stephanie", jabatan: "Bendahara Umum", foto: "assets/img/anggota/Shareen.jpg" }
+        ]
+    },
+    {
+        nama: "Hubungan Masyarakat",
+        deskripsi: "Departemen yang mengelola relasi dan komunikasi eksternal organisasi.",
+        kadep: { nama: "Samuel Supardjo", foto: "assets/img/anggota/Samuel.jpg" },
+        wakadep: { nama: "William Saputra", foto: "assets/img/anggota/William.jpg" },
+        anggota: [
+            { nama: "Kimberly Brigitte Kohdong", foto: "assets/img/anggota/Kimberly.jpg" },
+            { nama: "Angel Fransisca Wijaya", foto: "assets/img/anggota/Angel.jpg" },
+            { nama: "Filza Ardika", foto: "assets/img/anggota/Filza.jpg" }
+        ]
+    },
+    {
+        nama: "Kesekretariatan",
+        deskripsi: "Departemen yang menangani dokumentasi, arsip, dan administrasi.",
+        kadep: { nama: "Metta Levina", foto: "assets/img/anggota/Metta.jpg" },
+        wakadep: { nama: "Maria Melisa Herdwinanto", foto: "assets/img/anggota/Maria.jpg" },
+        anggota: [
+            { nama: "Veren Thiovanda", foto: "assets/img/anggota/Veren.jpg" },
+            { nama: "Louis Francis Tali", foto: "assets/img/anggota/Louis.jpg" },
+            { nama: "Irene Carl Lewis", foto: "assets/img/anggota/Irene.jpg" }
+        ]
+    },
+    {
+        nama: "Keuangan dan Kerjasama",
+        deskripsi: "Departemen yang mengelola anggaran dan menjalin kerja sama eksternal.",
+        kadep: { nama: "Richwill Ronaldy", foto: "assets/img/anggota/Richwill.jpg" },
+        wakadep: { nama: "Raymond Euginio Kent Sanjaya", foto: "assets/img/anggota/Raymond.jpg" },
+        anggota: [
+            { nama: "Eilleen", foto: "assets/img/anggota/Eilleen.jpg" },
+            { nama: "Stiven", foto: "assets/img/anggota/Stiven.jpg" }
+        ]
+    },
+    {
+        nama: "Pengabdian Masyarakat",
+        deskripsi: "Departemen yang berfokus pada kegiatan sosial dan pengabdian.",
+        kadep: { nama: "Love Hillary Swigo", foto: "assets/img/anggota/Love.jpg" },
+        wakadep: { nama: "Felix Ferdinad", foto: "assets/img/anggota/Felix.jpg" },
+        anggota: [
+            { nama: "Alexa Vega Aurellya", foto: "assets/img/anggota/Alexa.jpg" },
+            { nama: "Fionna Ameilia Aurita", foto: "assets/img/anggota/Fionna.jpg" }
+        ]
+    },
+    {
+        nama: "Media & Informasi",
+        deskripsi: "Departemen yang bertanggung jawab atas publikasi dan dokumentasi.",
+        kadep: { nama: "Ocean Charlie Gunawan", foto: "assets/img/anggota/Ocean.jpg" },
+        wakadep: { nama: "Vanessa Siemmanjaya", foto: "assets/img/anggota/Vanessa.jpg" },
+        anggota: [
+            { nama: "Hengky Laurencio", foto: "assets/img/anggota/Hengky.jpg" },
+            { nama: "Chandra Wijaya", foto: "assets/img/anggota/Chandra.jpg" },
+            { nama: "Gabriella Ignatia", foto: "assets/img/anggota/Gabriella.jpg" }
+        ]
+    },
+    {
+        nama: "Bakat & Minat",
+        deskripsi: "Departemen yang mengembangkan minat dan bakat mahasiswa.",
+        kadep: { nama: "Verlene Angelica", foto: "assets/img/anggota/Verlene.jpg" },
+        wakadep: { nama: "Brian", foto: "assets/img/anggota/Brian.jpg" },
+        anggota: [
+            { nama: "Michael Chan", foto: "assets/img/anggota/Michael.jpg" },
+            { nama: "Felix Jonathan", foto: "assets/img/anggota/Felix.jpg" },
+            { nama: "Brian Joe Lyon", foto: "assets/img/anggota/Brian Joe.jpg" }
+        ]
+    },
+    {
+        nama: "Akademik",
+        deskripsi: "Departemen yang mendukung kegiatan akademik dan pengembangan intelektual.",
+        kadep: { nama: "Paulina Agusia", foto: "assets/img/anggota/Paulina.jpg" },
+        wakadep: { nama: "Danniel", foto: "assets/img/anggota/Danniel.jpg" },
+        anggota: [
+            { nama: "Jocelyn Novia", foto: "assets/img/anggota/Jocelyn.jpg" },
+            { nama: "Lavisya Suwita", foto: "assets/img/anggota/Lavisya.jpg" },
+            { nama: "Ivara Sinatria Putri", foto: "assets/img/anggota/Ivara.jpg" },
+            { nama: "Stefi Lauren", foto: "assets/img/anggota/Stefi.jpg" }
+        ]
+    }
+];
+
+export default departemen;
