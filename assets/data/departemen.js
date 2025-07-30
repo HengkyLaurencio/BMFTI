239236fdb44c48da6120ec03d52a5557ad9b8fce
HengkyLaurencio <liu.hengky.laurencio@gmail.com>
2025-07-30 15:48:34 +0700

    Fix image url

--- assets/data/departemen.js
+++ assets/data/departemen.js
@@ -3,87 +3,87 @@ const departemen = [
         nama: "BPHI",
         deskripsi: "Badan Pengurus Harian Inti",
         anggota: [
-            { nama: "Nanda Goaw Putra", jabatan: "Ketua Umum", foto: "assets/img/anggota/Nanda.jpg" },
-            { nama: "Steven Flianto", jabatan: "Wakil Internal", foto: "assets/img/anggota/Steven.jpg" },
-            { nama: "Yudo Alfariszi", jabatan: "Wakil Eksternal", foto: "assets/img/anggota/Yudo.jpg" },
-            { nama: "Carissa Puteri", jabatan: "Sekretaris Umum", foto: "assets/img/anggota/Carissa.jpg" },
-            { nama: "Shareen Stephanie", jabatan: "Bendahara Umum", foto: "assets/img/anggota/Shareen.jpg" }
+            { nama: "Nanda Goaw Putra", jabatan: "Ketua Umum", foto: "assets/img/anggota/Nanda.JPG" },
+            { nama: "Steven Flianto", jabatan: "Wakil Internal", foto: "assets/img/anggota/Steven.JPG" },
+            { nama: "Yudo Alfariszi", jabatan: "Wakil Eksternal", foto: "assets/img/anggota/Yudo.JPG" },
+            { nama: "Carissa Puteri", jabatan: "Sekretaris Umum", foto: "assets/img/anggota/Carissa.JPG" },
+            { nama: "Shareen Stephanie", jabatan: "Bendahara Umum", foto: "assets/img/anggota/Shareen.JPG" }
         ]
     },
     {
         nama: "Hubungan Masyarakat",
         deskripsi: "Departemen yang mengelola relasi dan komunikasi eksternal organisasi.",
-        kadep: { nama: "Samuel Supardjo", foto: "assets/img/anggota/Samuel.jpg" },
-        wakadep: { nama: "William Saputra", foto: "assets/img/anggota/William.jpg" },
+        kadep: { nama: "Samuel Supardjo", foto: "assets/img/anggota/Samuel.JPG" },
+        wakadep: { nama: "William Saputra", foto: "assets/img/anggota/William.JPG" },
         anggota: [
-            { nama: "Kimberly Brigitte K.", foto: "assets/img/anggota/Kimberly.jpg" },
-            { nama: "Angel Fransisca Wijaya", foto: "assets/img/anggota/Angel.jpg" },
-            { nama: "Filza Ardika", foto: "assets/img/anggota/Filza.jpg" }
+            { nama: "Kimberly Brigitte K.", foto: "assets/img/anggota/Kimberly.JPG" },
+            { nama: "Angel Fransisca Wijaya", foto: "assets/img/anggota/Angel.JPG" },
+            { nama: "Filza Ardika", foto: "assets/img/anggota/Filza.JPG" }
         ]
     },
     {
         nama: "Kesekretariatan",
         deskripsi: "Departemen yang menangani dokumentasi, arsip, dan administrasi.",
-        kadep: { nama: "Metta Levina", foto: "assets/img/anggota/Metta.jpg" },
-        wakadep: { nama: "Maria Melisa H.", foto: "assets/img/anggota/Maria.jpg" },
+        kadep: { nama: "Metta Levina", foto: "assets/img/anggota/Metta.JPG" },
+        wakadep: { nama: "Maria Melisa H.", foto: "assets/img/anggota/Maria.JPG" },
         anggota: [
-            { nama: "Veren Thiovanda", foto: "assets/img/anggota/Veren.jpg" },
-            { nama: "Louis Francis Tali", foto: "assets/img/anggota/Louis.jpg" },
-            { nama: "Irene Carl Lewis", foto: "assets/img/anggota/Irene.jpg" }
+            { nama: "Veren Thiovanda", foto: "assets/img/anggota/Veren.JPG" },
+            { nama: "Louis Francis Tali", foto: "assets/img/anggota/Louis.JPG" },
+            { nama: "Irene Carl Lewis", foto: "assets/img/anggota/Irene.JPG" }
         ]
     },
     {
         nama: "Keuangan dan Kerjasama",
         deskripsi: "Departemen yang mengelola anggaran dan menjalin kerja sama eksternal.",
-        kadep: { nama: "Richwill Ronaldy", foto: "assets/img/anggota/Richwill.jpg" },
-        wakadep: { nama: "Raymond Euginio K. S.", foto: "assets/img/anggota/Raymond.jpg" },
+        kadep: { nama: "Richwill Ronaldy", foto: "assets/img/anggota/Richwill.JPG" },
+        wakadep: { nama: "Raymond Euginio K. S.", foto: "assets/img/anggota/Raymond.JPG" },
         anggota: [
-            { nama: "Eilleen", foto: "assets/img/anggota/Eilleen.jpg" },
-            { nama: "Stiven", foto: "assets/img/anggota/Stiven.jpg" }
+            { nama: "Eilleen", foto: "assets/img/anggota/Eilleen.JPG" },
+            { nama: "Stiven", foto: "assets/img/anggota/Stiven.JPG" }
         ]
     },
     {
         nama: "Pengabdian Masyarakat",
         deskripsi: "Departemen yang berfokus pada kegiatan sosial dan pengabdian.",
-        kadep: { nama: "Love Hillary Swigo", foto: "assets/img/anggota/Love.jpg" },
-        wakadep: { nama: "Felix Ferdinad", foto: "assets/img/anggota/Felix.jpg" },
+        kadep: { nama: "Love Hillary Swigo", foto: "assets/img/anggota/Love.JPG" },
+        wakadep: { nama: "Felix Ferdinad", foto: "assets/img/anggota/Felix.JPG" },
         anggota: [
-            { nama: "Alexa Vega Aurellya", foto: "assets/img/anggota/Alexa.jpg" },
-            { nama: "Fionna Ameilia Aurita", foto: "assets/img/anggota/Fionna.jpg" }
+            { nama: "Alexa Vega Aurellya", foto: "assets/img/anggota/Alexa.JPG" },
+            { nama: "Fionna Ameilia Aurita", foto: "assets/img/anggota/Fionna.JPG" }
         ]
     },
     {
         nama: "Media & Informasi",
         deskripsi: "Departemen yang bertanggung jawab atas publikasi dan dokumentasi.",
-        kadep: { nama: "Ocean Charlie Gunawan", foto: "assets/img/anggota/Ocean.jpg" },
-        wakadep: { nama: "Vanessa Siemmanjaya", foto: "assets/img/anggota/Vanessa.jpg" },
+        kadep: { nama: "Ocean Charlie Gunawan", foto: "assets/img/anggota/Ocean.JPG" },
+        wakadep: { nama: "Vanessa Siemmanjaya", foto: "assets/img/anggota/Vanessa.JPG" },
         anggota: [
-            { nama: "Hengky Laurencio", foto: "assets/img/anggota/Hengky.jpg" },
-            { nama: "Chandra Wijaya", foto: "assets/img/anggota/Chandra.jpg" },
-            { nama: "Gabriella Ignatia", foto: "assets/img/anggota/Gabriella.jpg" }
+            { nama: "Hengky Laurencio", foto: "assets/img/anggota/Hengky.JPG" },
+            { nama: "Chandra Wijaya", foto: "assets/img/anggota/Chandra.JPG" },
+            { nama: "Gabriella Ignatia", foto: "assets/img/anggota/Gabriella.JPG" }
         ]
     },
     {
         nama: "Bakat & Minat",
         deskripsi: "Departemen yang mengembangkan minat dan bakat mahasiswa.",
-        kadep: { nama: "Verlene Angelica", foto: "assets/img/anggota/Verlene.jpg" },
-        wakadep: { nama: "Brian", foto: "assets/img/anggota/Brian.jpg" },
+        kadep: { nama: "Verlene Angelica", foto: "assets/img/anggota/Verlene.JPG" },
+        wakadep: { nama: "Brian", foto: "assets/img/anggota/Brian.JPG" },
         anggota: [
-            { nama: "Michael Chan", foto: "assets/img/anggota/Michael.jpg" },
-            { nama: "Felix Jonathan", foto: "assets/img/anggota/Felix J.jpg" },
-            { nama: "Brian Joe Lyon", foto: "assets/img/anggota/Brian J.jpg" }
+            { nama: "Michael Chan", foto: "assets/img/anggota/Michael.JPG" },
+            { nama: "Felix Jonathan", foto: "assets/img/anggota/Felix J.JPG" },
+            { nama: "Brian Joe Lyon", foto: "assets/img/anggota/Brian J.JPG" }
         ]
     },
     {
         nama: "Akademik",
         deskripsi: "Departemen yang mendukung kegiatan akademik dan pengembangan intelektual.",
-        kadep: { nama: "Paulina Agusia", foto: "assets/img/anggota/Paulina.jpg" },
-        wakadep: { nama: "Danniel", foto: "assets/img/anggota/Danniel.jpg" },
+        kadep: { nama: "Paulina Agusia", foto: "assets/img/anggota/Paulina.JPG" },
+        wakadep: { nama: "Danniel", foto: "assets/img/anggota/Danniel.JPG" },
         anggota: [
-            { nama: "Jocelyn Novia", foto: "assets/img/anggota/Jocelyn.jpg" },
-            { nama: "Lavisya Suwita", foto: "assets/img/anggota/Lavisya.jpg" },
-            { nama: "Ivara Sinatria Putri", foto: "assets/img/anggota/Ivara.jpg" },
-            { nama: "Stefi Lauren", foto: "assets/img/anggota/Stefi.jpg" }
+            { nama: "Jocelyn Novia", foto: "assets/img/anggota/Jocelyn.JPG" },
+            { nama: "Lavisya Suwita", foto: "assets/img/anggota/Lavisya.JPG" },
+            { nama: "Ivara Sinatria Putri", foto: "assets/img/anggota/Ivara.JPG" },
+            { nama: "Stefi Lauren", foto: "assets/img/anggota/Stefi.JPG" }
         ]
     }
 ];
